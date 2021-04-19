591d51ce0123e14f2e31d3c74e1b22921a676143
mevlutbucan <59297282+mevlutbucan@users.noreply.github.com>
2021-04-19 18:30:51 +0000

    updated get-dealerships action

--- functions/get-dealerships.js
+++ functions/get-dealerships.js
@@ -9,33 +9,30 @@
  *  - **_include_docs_** (boolean): Include the full content of the design documents in the return. Default is `false`.
  *  - **_limit_** (number): Limit the number of the returned design documents to the specified number. `Optional`
  **/
-function main(message) {
-    if (!message.host) return Promise.reject('host parameter is required.');
-    if (!message.iamApiKey) return Promise.reject('iamApiKey parameter is required.');
-    if (!message.iamUrl) return Promise.reject('iamUrl parameter is required.');
-    const cloudant = getCloudantAccount(message);
-    if (!message.dbName) return Promise.reject('dbName parameter is required.');
-    const cloudantDb = cloudant.db.use(message.dbName);
-    return listAllDocuments(cloudantDb, message.params);
-}
-
-/**
- * List all documents.
- */
-function listAllDocuments(cloudantDb, params) {
-    return new Promise(function (resolve, reject) {
-        cloudantDb.list(params, function (error, response) {
-            if (!error) {
-                resolve(response);
-            } else {
-                console.error("error", error);
-                reject(error);
+async function main(params) {
+    const cloudant = getCloudantAccount(params);
+    const cloudantDb = cloudant.db.use('dealerships');
+    const list = await cloudantDb.list({ include_docs: true })
+    return {
+        entries: list.rows.map((row) => {
+            return {
+                id: row.doc.id,
+                city: row.doc.city,
+                state: row.doc.state,
+                st: row.doc.st,
+                address: row.doc.address,
+                zip: row.doc.zip,
+                lat: row.doc.lat,
+                long: row.doc.long
             }
-        });
-    });
+        })
+    };
 }
 
 function getCloudantAccount(params) {
+    if (!params.host) return Promise.reject('host parameter is required.');
+    if (!params.iamApiKey) return Promise.reject('iamApiKey parameter is required.');
+    if (!params.iamUrl) return Promise.reject('iamUrl parameter is required.');
     const Cloudant = require('@cloudant/cloudant');
     return new Cloudant({
         url: `https://${params.host}`,
